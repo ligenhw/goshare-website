4ffe8db96f5996513e3b95c3c51b5d4d14bec45e
l00273743 <ligen1@huawei.com>
2019-05-07 18:18:40 +0800

    add test for route

--- src/router/index.test.js
+++ src/router/index.test.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../App';
-import { mainPages } from './index'
+import allPages from './index'
 import { history } from '../store/configureStore'
 
 it('route all pages', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
-  mainPages.forEach( page => {
+  allPages.forEach( page => {
       history.push(page.path)
   })
   ReactDOM.unmountComponentAtNode(div);
