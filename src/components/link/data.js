9929b7070cffed3f56fe23fa32c028b9f3467fb8
gen <ligenhw@outlook.com>
2019-05-27 20:59:10 +0800

    add acheve add links

--- src/components/link/data.js
+++ src/components/link/data.js
@@ -1,12 +1,17 @@
 export default [
     {
+        "name": "Thinking in code",
+        "url": "https://www.bestlang.cn/",
+        "img": "https://avatars3.githubusercontent.com/u/7815460?s=460&v=4"
+    },
+    {
         "name": "goshare-website",
-        "url": "https://github.com/ligenhw/goshare",
+        "url": "https://github.com/ligenhw/goshare-website",
         "img": "https://avatars3.githubusercontent.com/u/7815460?s=460&v=4"
     },
     {
         "name": "goshare",
-        "url": "https://github.com/ligenhw/goshare-website",
+        "url": "https://github.com/ligenhw/goshare",
         "img": "https://avatars3.githubusercontent.com/u/7815460?s=460&v=4"
-    },
+    }
 ]
