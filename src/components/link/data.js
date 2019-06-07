182a737e3540e4ab9fd2a1e596947d5e49638b76
gen <ligenhw@outlook.com>
2019-06-08 00:37:28 +0800

    add links

--- src/components/link/data.js
+++ src/components/link/data.js
@@ -18,5 +18,20 @@ export default [
         "name": "Szx's Blog",
         "url": "https://songzixian.com/",
         "img": "https://cdn.sinaimg.cn.52ecy.cn/large/005BYqpgly1g2wgcd57ctj30hs0hsjsc.jpg"
+    },
+    {
+        "name": "杨学峰博客",
+        "url": "https://www.yangsihan.com",
+        "img": "https://www.yangsihan.com/static/css/logo.png"
+    },
+    {
+        "name": "Suummmmer",
+        "url": "https://blog.thinker.ink",
+        "img": "https://blog.thinker.ink/static/images/avatar.jpg"
+    },
+    {
+        "name": "曾中杰's world",
+        "url": "https://www.zengzhongjie.com",
+        "img": "https://www.zengzhongjie.com/images/thelittleprince.jpg"
     }
 ]
