f9ecc6c8b52a8414db7352750d6691873042c6e4
gen <ligenhw@outlook.com>
2019-06-25 12:21:58 +0800

    fix link

--- src/components/link/data.js
+++ src/components/link/data.js
@@ -1,23 +1,8 @@
 export default [
     {
-        "name": "Thinking in code",
-        "url": "https://www.bestlang.cn/",
-        "img": "https://avatars3.githubusercontent.com/u/7815460?s=460&v=4"
-    },
-    {
-        "name": "goshare-website",
-        "url": "https://github.com/ligenhw/goshare-website",
-        "img": "https://avatars3.githubusercontent.com/u/7815460?s=460&v=4"
-    },
-    {
-        "name": "goshare",
-        "url": "https://github.com/ligenhw/goshare",
-        "img": "https://avatars3.githubusercontent.com/u/7815460?s=460&v=4"
-    },
-    {
-        "name": "Szx's Blog",
-        "url": "https://songzixian.com/",
-        "img": "https://cdn.sinaimg.cn.52ecy.cn/large/005BYqpgly1g2wgcd57ctj30hs0hsjsc.jpg"
+        "name": "Ryan0up'S Blog",
+        "url": "https://ryanc.cc",
+        "img": "https://q1.qlogo.cn/g?b=qq&nk=709831589&s=5"
     },
     {
         "name": "杨学峰博客",
@@ -35,11 +20,6 @@ export default [
         "img": "https://www.zengzhongjie.com/images/thelittleprince.jpg"
     },
     {
-        "name": "Ryan0up'S Blog",
-        "url": "https://ryanc.cc",
-        "img": "https://q1.qlogo.cn/g?b=qq&nk=709831589&s=5"
-    },
-    {
         "name": "低调小熊猫",
         "url": "https://aodeng.cc",
         "img": "https://avatars1.githubusercontent.com/u/35907367?v=4"
@@ -53,5 +33,10 @@ export default [
         "name": "水寒的博客",
         "url": "https://dp2px.com",
         "img": "https://dp2px.com/images/download_head.png"
+    },
+    {
+        "name": "Airing 的小屋",
+        "url": "https://me.ursb.me",
+        "img": "https://airing.ursb.me/image/airing-face.png"
     },
 ]
